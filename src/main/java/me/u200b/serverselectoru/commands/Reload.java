fa8abbf2b8065ea1f3c9c2a3a1cce71602614430
u200b <u200b@doverman.nl>
2022-03-18 20:27:36 +0100

    Fix: #2

--- src/main/java/me/u200b/serverselectoru/commands/Reload.java
+++ src/main/java/me/u200b/serverselectoru/commands/Reload.java
@@ -8,6 +8,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.io.IOException;
+
 public class Reload implements CommandExecutor {
     ServerSelectorU plugin;
     public Reload(ServerSelectorU instance) {
@@ -21,8 +23,16 @@ public class Reload implements CommandExecutor {
             if (player.hasPermission("ss.reload")) {
                 if (args.length >= 1) {
                     if (args[0].equalsIgnoreCase("reload") || args[0].equalsIgnoreCase("rl")) {
-
-                        player.sendMessage(PlaceholderAPI.setPlaceholders(player, ChatColor.translateAlternateColorCodes('&', plugin.messageConfig.getString("reload"))));
+                        try {
+                            plugin.messageConfig.load(plugin.messageFile);
+                            plugin.menuConfig.load(plugin.menuFile);
+                            plugin.configConfig.load(plugin.configFile);
+                            player.sendMessage(PlaceholderAPI.setPlaceholders(player, ChatColor.translateAlternateColorCodes('&', plugin.messageConfig.getString("reload"))));
+                            return true;
+                        } catch (Exception e) {
+                            plugin.getLogger().warning("Reload failed: " + e.getMessage());
+                            return false;
+                        }
                     }
                 } else {
                     player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.messageConfig.getString("usage")));
